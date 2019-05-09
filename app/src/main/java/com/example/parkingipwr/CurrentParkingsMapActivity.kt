b1e4787dbd18c2b854a6315c8c9ad90b103c4e84
Paulina <p.jedrzejewska97@gmail.com>
2019-05-09 17:27:07 +0200

    GoogleMap with marker added to CurrentParkingsStateActivity. MinSDK changed to 18

--- app/src/main/java/com/example/parkingipwr/CurrentParkingsMapActivity.kt
+++ app/src/main/java/com/example/parkingipwr/CurrentParkingsMapActivity.kt
@@ -2,11 +2,40 @@ package com.example.parkingipwr
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.CameraUpdate
 
-class CurrentParkingsMapActivity : AppCompatActivity() {
+
+
+
+
+class CurrentParkingsMapActivity : AppCompatActivity(), OnMapReadyCallback {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_current_parkings_map)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+
+        val mark = LatLng(51.107064, 17.058980)
+
+        val yourLocation = CameraUpdateFactory.newLatLngZoom(mark, 18f)
+
+        googleMap!!.addMarker(
+            MarkerOptions().position(mark)
+                .title("Marker C-13")
+        )
+        googleMap!!.animateCamera(yourLocation)
     }
 }
