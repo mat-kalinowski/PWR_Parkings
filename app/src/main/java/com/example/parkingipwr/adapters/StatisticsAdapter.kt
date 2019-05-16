26c4a537fac8815641c7f1d50de7eb493364c166
Paulina <p.jedrzejewska97@gmail.com>
2019-05-16 18:10:27 +0200

    Statistics activity design

--- app/src/main/java/com/example/parkingipwr/adapters/StatisticsAdapter.kt
+++ app/src/main/java/com/example/parkingipwr/adapters/StatisticsAdapter.kt
@@ -2,6 +2,7 @@ package com.example.parkingipwr.adapters
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,6 +16,11 @@ import com.github.mikephil.charting.components.Description
 import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.utils.ColorTemplate
 import org.w3c.dom.Text
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+
+
+
 
 class StatisticsAdapter(private val context: Context, private val data : MutableList<StatisticsObject>) : BaseAdapter(){
 
@@ -50,22 +56,28 @@ class StatisticsAdapter(private val context: Context, private val data : Mutable
             )
         }
 
+
+
         val dataSet = BarDataSet(entries, "")
         dataSet.color = R.color.colorText
-        dataSet.valueTextColor = R.color.colorText
-
-        //dataSet.setColors(ColorTemplate.COLORFUL_COLORS)
+        dataSet.valueTextColor = R.color.colorBackground
         dataSet.isHighlightEnabled = false
 
         val lineData = BarData(dataSet)
+        var colorTemplate: IntArray = IntArray(3)
+        colorTemplate[0] = R.color.barChartColor1
+        colorTemplate[1] = R.color.barChartColor2
+        colorTemplate[2] = R.color.barChartColor3
+        dataSet.setColors(colorTemplate, context)
 
         val chartObj = statisticItem.findViewById<BarChart>(R.id.chart)
         chartObj.data = lineData
+
         chartObj.invalidate()
         chartObj.description.text = "ilość miejsc"
-
-        chartObj.description.textColor = context.resources.getColor(R.color.colorText)
-        chartObj.setBackgroundColor(context.resources.getColor(R.color.colorBackground))
+        chartObj.barData.setValueTextColor(context.resources.getColor(R.color.colorText))
+        chartObj.description.textColor = context.resources.getColor(R.color.colorBackground)
+        chartObj.setBackgroundColor(context.resources.getColor(R.color.colorText))
 
         statisticItem.findViewById<TextView>(R.id.thirdLine).text = lastFreeSlot
 
